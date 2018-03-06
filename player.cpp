aa8506033a850b8b6dec5b76951e8cd2232a0801
cs2user <ankush.hommerichdutt@gmail.com>
2018-03-06 01:02:35 -0800

    minimax works./testminimax

--- player.cpp
+++ player.cpp
@@ -55,8 +55,8 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */
-     std::vector<Move *> possibleMoves;
-     std::vector<int> scores;
+
+    /*
 
      time_t begin_time;
      time_t curr_time;
@@ -72,70 +72,44 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      if (timeLimit == 1) {
         time(&begin_time);
      }
+     */ 
 
-
+    int depth = 4;
+    Move* best_move = new Move(0,0);
 
      newBoard->doMove(opponentsMove, theirSide);
+
+
      if(!newBoard->hasMoves(mySide) || newBoard->isDone())
      {
          return nullptr;
      }
-     Move *mine = new Move(1, 1);
 
-     for(int i = 0; i < 8; i++)
-     {
-         for(int j = 0; j < 8; j++)
-         {
-             if (timeLimit == 1) {
-                time(&curr_time);
-                diff_time = difftime(curr_time, begin_time);
-
-                if (diff_time > (double) (msLeft - 1)) {
-                    return nullptr;
-                }
-             }
-
-             mine->setX(i);
-             mine->setY(j);
-             if(newBoard->checkMove(mine, mySide))
-             {
-                 Move *addmine = new Move(i, j);
-                 possibleMoves.push_back(addmine);
-
-                 int s = getScore(addmine);
-                 scores.push_back(s);
-             }
-         }
-     }
-     int max = -10000;
-     int index;
-     for(unsigned int i = 0; i < scores.size(); i++)
-     {
-         if(scores[i] > max)
-         {
-             max = scores[i];
-             index = i;
-         }
+     if (testingMinimax) {
+        depth = 2;
      }
 
-     newBoard->doMove(possibleMoves[index], mySide);
-     return possibleMoves[index];
 
+
+     best_move = heuristic(depth);
+
+     newBoard->doMove(best_move, mySide);
+     return best_move;
 
 }
 
-int Player::getScore(Move *currentmove)
+int Player::getScore(Move *currentmove, Board* &current_board)
 {
-    Board *temp = newBoard->copy();
-    newBoard->doMove(currentmove, mySide);
+    Board *temp = current_board->copy();
+    current_board->doMove(currentmove, mySide);
     int score;
     if(mySide == WHITE)
     {
-        score = newBoard->countWhite() - newBoard->countBlack();
+        score = current_board->countWhite() - current_board->countBlack();
     }
     else
     {
-        score = newBoard->countBlack() - newBoard->countWhite();
+        score = current_board->countBlack() - current_board->countWhite();
     }
 
     //check if corner, dark green
@@ -162,6 +136,127 @@ int Player::getScore(Move *currentmove)
     {
         score = score * 2;
     }
-    newBoard = temp;
+    current_board = temp;
     return score;
+}
+
+int Player::minimax(Move *currentmove, int depth, bool maximizingPlayer, Board* current_board) {
+    
+
+    std::vector<Move *> possibleMoves, subset;
+    possibleMoves = generate_moves(currentmove, maximizingPlayer, current_board);
+    int best;
+
+    if ((possibleMoves.size() == 0) || (depth == 0)) {
+        return getScore(currentmove, current_board);
+    }
+
+    if (maximizingPlayer) {
+        int bestValue = -10000;
+        for (unsigned int i = 0; i < possibleMoves.size(); i++) {
+            best = minimax(possibleMoves[i], depth - 1, !maximizingPlayer, current_board);
+        }
+        if (best > bestValue) {
+            return best;
+        }
+        return bestValue;
+    }
+
+    else {
+        int bestValue = 100000;
+        for (unsigned int i = 0; i < possibleMoves.size(); i++) {
+            best = minimax(possibleMoves[i], depth - 1, !maximizingPlayer, current_board);
+        }
+        if (best < bestValue) {
+            return best;
+        }
+        return bestValue;
+    }
+
+}
+
+Move * Player::heuristic(int depth) {
+    std::vector<Move *> possibleMoves;
+    std::vector<int> scores;    
+    Move *mine = new Move(1, 1);
+    int index;
+
+     for(int i = 0; i < 8; i++)
+     {
+         for(int j = 0; j < 8; j++)
+         {
+             /*
+             if (timeLimit == 1) {
+                time(&curr_time);
+                diff_time = difftime(curr_time, begin_time);
+
+                if (diff_time > (double) (msLeft - 1)) {
+                    return nullptr;
+                }
+             }
+             */
+
+             mine->setX(i);
+             mine->setY(j);
+             if(newBoard->checkMove(mine, mySide))
+             {
+                 Move *addmine = new Move(i, j);
+                 possibleMoves.push_back(addmine);
+                 Board *temp = new Board;
+                 temp = newBoard->copy();
+                 scores.push_back(minimax(possibleMoves.back(), depth, true, temp));
+             }
+         }
+     }
+
+     int max = -10000;
+     for (unsigned int i = 0; i < scores.size(); i++) {
+        if (scores[i] > max) {
+            max = scores[i];
+            index = i;
+        }
+     }
+
+     return possibleMoves[index];
+}
+
+std::vector <Move *> Player::generate_moves(Move *currentMove, bool max_or_min, Board* &current_board) {
+    
+    if (max_or_min) {
+        current_board->doMove(currentMove, mySide);   
+    }
+    else {
+        current_board->doMove(currentMove, theirSide);
+    }
+
+    std::vector<Move *> possibleMoves;
+    Move *mine = new Move(1, 1);
+
+     for(int i = 0; i < 8; i++)
+     {
+         for(int j = 0; j < 8; j++)
+         {
+             /*
+             if (timeLimit == 1) {
+                time(&curr_time);
+                diff_time = difftime(curr_time, begin_time);
+
+                if (diff_time > (double) (msLeft - 1)) {
+                    return nullptr;
+                }
+             }
+             */
+
+             mine->setX(i);
+             mine->setY(j);
+             if(current_board->checkMove(mine, mySide))
+             {
+                 Move *addmine = new Move(i, j);
+                 possibleMoves.push_back(addmine);
+             }
+         }
+     }
+
+    return possibleMoves;
+
 }
--- player.hpp
+++ player.hpp
@@ -2,6 +2,7 @@
 #define __PLAYER_H__
 
 #include <iostream>
+#include <vector>
 #include "common.hpp"
 #include "board.hpp"
 using namespace std;
@@ -14,7 +15,10 @@ public:
 
 
     Move *doMove(Move *opponentsMove, int msLeft);
-    int getScore(Move *currentmove);
+    int getScore(Move *currentmove, Board* &current_board);
+    int minimax(Move *currentmove, int depth, bool maximizingPlayer, Board* current_board);
+    Move * heuristic(int depth);
+    std::vector<Move *> generate_moves(Move *opponentsMove, bool max, Board* &current_board);
 
     // Flag to tell if the player is running within the test_minimax context
     bool testingMinimax;
